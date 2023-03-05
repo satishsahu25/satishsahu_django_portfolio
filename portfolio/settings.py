703989f61859cfa8a5942f536f776271dbda99ca
Satish Sahu <satishk7619@gmail.com>
2023-03-05 22:54:50 +0530

    vercel

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -14,6 +14,7 @@ SECRET_KEY = 'django-insecure--0p0*hi$jak-)gjsujlf@1aswyzc2t$5a*zr$d7vy@jo(c@-r9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
@@ -105,10 +106,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-
-import os
 STATIC_URL = 'static/'
-STATICFILES_DIRS=os.path.join(BASE_DIR ,'static')
+import os
+
+STATICFILES_DIRS=[os.path.join(BASE_DIR ,'static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 
 MEDIA_URL="/media/"
